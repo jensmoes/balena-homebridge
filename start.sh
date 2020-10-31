a09ff87c636428bce3c4cc24ce7ac9b43cfc694b
Jens Troest <jens@troest.com>
2020-10-31 08:30:52 -0700

    Get rid of systemd hb-service launch madness Migrate homebridge dir to persistent storage Log to non-persistent memory to limit SD wear Send automated backups to network drive

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Tell the container that DBUS should report to Host OS
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-export SYSTEMD_LOG_LEVEL=info
 
 # Set hostname default
 if [[ -z "$DEVICE_HOSTNAME" ]]; then
@@ -14,4 +13,12 @@ curl -X PATCH --header "Content-Type:application/json" \
     --data '{"network": {"hostname": "'"${DEVICE_HOSTNAME}"'"}}' \
     "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
 
-sleep infinity
+# Mount the backup destination. TODO: make this automount
+mkdir -p /mnt/storage
+mount -t cifs //PecanPi.lan/Storage/Homebridge /mnt/storage -o rw,username=Guest,password=
+# Put the homebridge directory in persistent mem
+mkdir -p /data/homebridge/
+cp /usr/src/*.json /data/homebridge/
+
+echo Starting homebridge
+exec homebridge --user-storage-path /data/homebridge/ > /var/log/homebridge.log
